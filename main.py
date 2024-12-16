44b691db50f595a40a9c07c17a328cec4f948661
Jetson4112 <jetson4112@gmail.com>
2024-12-16 12:33:19 +0530

    Added basic functionality to script     made addition practice

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("testing github out")
+#/////IMPORT STATEMENTS/////#
+import random
+import time
+print("this is addition pracitce script")
+#time.sleep(2)
+#print()
+#print(random.randint(0,10)) #DEBUG --> generating random numbers 
+
+
+def addScript ():
+    one = random.randint(0,10)
+    two = random.randint(0,10)
+    ans = one + two
+    if ans == int(input("What is " + str(one) + " + " + str(two) + " ?")):
+        print("correct")
+        passed = True
+    else:
+        print("wrong")
+        passed = False
+    return(passed)
+print(addScript())
