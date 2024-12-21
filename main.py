11e18035476fadc6c041d3c14d2edcab284e8e4c
Jetson4112 <jetson4112@gmail.com>
2024-12-21 17:24:28 +0530

    Update main.py
    testing timer functionality

--- main.py
+++ main.py
@@ -6,16 +6,27 @@ print("this is addition pracitce script")
 #print()
 #print(random.randint(0,10)) #DEBUG --> generating random numbers 
 
+def timer( start: int, endVal: int, step :int, timerMessage : str, endMessage : str) :
+    for i in range(start, endVal, step):
+        print(timerMessage, str(i), end = "\r")
+        time.sleep(abs(step))
+    print(" " * (len(timerMessage) + 5))
+    print(endMessage, "\n")
+timer(3, 0, -1, "Clock is ticking!!!", "Time is up!!!")
 
 def addScript ():
     one = random.randint(0,10)
     two = random.randint(0,10)
     ans = one + two
-    if ans == int(input("What is " + str(one) + " + " + str(two) + " ?")):
+    print(inAns + "<--- inAns")
+    inAns = input("What is {one} + {two} + ?")
+    """timer(3, 0, -1, "Clock is ticking!!!", "Time is up!!")
+    if ans == int(inAns):
         print("correct")
         passed = True
     else:
         print("wrong")
         passed = False
-    return(passed)
+    return(passed)"""
+addScript()
 #print(addScript())#DEBUG
