7bb0ee212701a632a4d186d403c138bdbab032b4
Jetson4112 <jetson4112@gmail.com>
2024-12-27 21:57:53 +0530

    added flow debug functionality

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import time
 import random
 import threading
-debugMode = False
+debugMode = False #set to true to enable flow control print statements
 passed = threading.Event()
+timerEnd = threading.Event()
 
 def addScript ():
-    one = random.randint(0,10)
-    two = random.randint(0,10)
-    #one, two = 1, 2 #uncomment for debug
+    if debugMode:
+        one, two = 1, 2 
+        pass 
+    else:
+        one = random.randint(0,10)
+        two = random.randint(0,10)
+        pass
+
     ans = one + two
     inAns = input("What is " + str(one) + " + " + str(two) + " + ?")
-    #print(inAns)#uncomment for debug
+    if debugMode:
+        print(inAns)
+        pass 
+    else:
+        pass
     if ans == int(inAns):
         print("correct")
         passed.set()
@@ -24,22 +34,43 @@ def timer( start: int, endVal: int, step :int, timerMessage : str, endMessage : 
     print("\n")
     try:
         for i in range(start, endVal, step):
-            #print("\n=======================================================\n")#uncomment for debug
+            if debugMode:
+                print("\n=======================================================\n")
+                pass 
+            else:
+                pass
             if (passed.is_set()):
-                #print("\ntriggered exit in timer\n")#uncomment for debug
+                ##uncomment for debug
+                if debugMode:
+                    print("\ntriggered exit in timer\n")
+                    pass 
+                else:
+                    pass
                 raise SystemExit
                 #exit here
             else:
-                #print("\nchecking passed........................")
+                if debugMode:
+                    print("\nchecking passed........................")
+                    pass 
+                else:
+                    pass
                 pass    
-            #print("passed " + str(passed))         #uncomment for debug
+            if debugMode:
+                print("passed " + str(passed))
+                pass 
+            else:
+                pass
             print(timerMessage, str(i), end = "\r")
             time.sleep(abs(step))
         if (not passed.is_set()):
             print(" " * (len(timerMessage) + 5))
             print(endMessage, "\n")
     except SystemExit:
-        #print("timer ended")       #uncomment for debug
+        if debugMode:
+            print("timer ended")
+            pass 
+        else:
+            pass
         return
 def inDebug(): 
     i = input("debug mode")
