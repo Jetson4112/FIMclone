e7e31d0b2ec5d19178825fa1ef00dc1983830a94
Jetson4112 <jetson4112@gmail.com>
2024-12-27 21:27:50 +0530

    addition practice with timer functionality       implemented     automatic exit of timer function

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
-#/////IMPORT STATEMENTS/////#
-import random
 import time
-print("this is addition pracitce script")
-#time.sleep(2)
-#print()
-#print(random.randint(0,10)) #DEBUG --> generating random numbers 
-
-def timer( start: int, endVal: int, step :int, timerMessage : str, endMessage : str) :
-    for i in range(start, endVal, step):
-        print(timerMessage, str(i), end = "\r")
-        time.sleep(abs(step))
-    print(" " * (len(timerMessage) + 5))
-    print(endMessage, "\n")
-timer(3, 0, -1, "Clock is ticking!!!", "Time is up!!!")
+import random
+import threading
+debugMode = False
+passed = threading.Event()
 
 def addScript ():
     one = random.randint(0,10)
     two = random.randint(0,10)
+    #one, two = 1, 2 #uncomment for debug
     ans = one + two
-    print(inAns + "<--- inAns")
-    inAns = input("What is {one} + {two} + ?")
-    """timer(3, 0, -1, "Clock is ticking!!!", "Time is up!!")
+    inAns = input("What is " + str(one) + " + " + str(two) + " + ?")
+    #print(inAns)#uncomment for debug
     if ans == int(inAns):
         print("correct")
-        passed = True
+        passed.set()
     else:
         print("wrong")
-        passed = False
-    return(passed)"""
-addScript()
-#print(addScript())#DEBUG
+        passed.clear()
+    return(passed)
+
+def timer( start: int, endVal: int, step :int, timerMessage : str, endMessage : str) :
+    print("\n")
+    print("\n")
+    try:
+        for i in range(start, endVal, step):
+            #print("\n=======================================================\n")#uncomment for debug
+            if (passed.is_set()):
+                #print("\ntriggered exit in timer\n")#uncomment for debug
+                raise SystemExit
+                #exit here
+            else:
+                #print("\nchecking passed........................")
+                pass    
+            #print("passed " + str(passed))         #uncomment for debug
+            print(timerMessage, str(i), end = "\r")
+            time.sleep(abs(step))
+        if (not passed.is_set()):
+            print(" " * (len(timerMessage) + 5))
+            print(endMessage, "\n")
+    except SystemExit:
+        #print("timer ended")       #uncomment for debug
+        return
+def inDebug(): 
+    i = input("debug mode")
+    if i != "":
+        exit()
+
+threadTimer = threading.Thread(target = timer, args=(5, 0, -1, "Clock is ticking!!!", "Time is up!!"))
+threadScript = threading.Thread(target=addScript)
+threadInDebug = threading.Thread(target = inDebug)
+threadScript.start()
+#threadInDebug.start()
+threadTimer.start()
