b3bf811f6d92da481607befa2ac9abf8a9ec3560
Jetson4112 <jetson4112@gmail.com>
2025-05-01 13:46:04 +0530

    screens is using logging library  - each invoked function has a log message  - eached ending has a log message  - inVal values are logged
    general cleanup
     - extra indents removed
     - unneded imports removed from screens.py
     - capitalisation in menu screen fixed

--- screens.py
+++ screens.py
@@ -1,16 +1,18 @@
-import os, time
+import os, time, logging, logging.config
 
 import var
 
-from var import score
+#from var import score #deemed unneded
 
-
+logging.config.fileConfig("logConfig.conf")
+log = logging.getLogger("screensLogger")
 def clear():
-    
-        if os.name == 'nt':
-            _ = os.system('cls')# For Windows
-        else:
-            _ = os.system('clear')# For macOS and Linux
+    log.debug("triggered clear()")
+    if os.name == 'nt':
+        _ = os.system('cls')# For Windows
+    else:
+        _ = os.system('clear')# For macOS and Linux
+    log.info("cleared screen")
 
 def intro():
     clear()
@@ -18,23 +20,25 @@ def intro():
     """Welcome to Arithmetics Range. this app is still a WIP. Report bugs on the GitHub page.
     
     Enter the Numbers to select your input;
-    the game will start as soon as you press enter with your choice
+    The game will start as soon as you press enter with your choice
         1. Addition
         2. Subtraction
         3. Multiplication
     reset. reset the main menu
      quit. quit this program""")
     time.sleep(1)
-    
+    log.debug("printed menu")
     var.inVal = input("\nEnter your response-->")
+    log.info(f"inVal = {var.inVal}")
 
 
 def endScreen():
     time.sleep(1)
     clear()
-    print(f" your score was {var.score}")
+    print(f"your score was {var.score}")
     print("\nthanks for playing!!")
     time.sleep(1)
+    log.info("ended execution")
 
 if __name__ == "__main__":
     intro()
