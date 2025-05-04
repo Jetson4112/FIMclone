04410e349ea5c76befc8888304ebbd93f54844ca
Jetson4112 <jetson4112@gmail.com>
2025-05-04 19:53:07 +0530

    timer.py updated to use logging
    ==========ADDITIONS==========
    
    NONE
    
    ===========MODIFIED============
    
    timer.py
    - "if debugMode" statements are replaced with logging
    - except block modified to use logging

--- scripts/timer.py
+++ scripts/timer.py
@@ -5,8 +5,11 @@ import var
 #this code is a mess
 #id greatly appreciate cleaning it up a bit
 
-from var import passed, failed, debugMode
+from var import passed, failed #debugMode #debugMode is no longer used
 
+from logConfig import logging
+
+log = logging.getLogger("timerLogger")
 
 def timer(
 endVal: int = 0, 
@@ -23,50 +26,32 @@ endMessage : str = "Timer ended. Press ENTER to continue:") :
         print("\n")
         try:
             for i in range(start, endVal, step):
-                if debugMode:
-                    print("\n=======================================================\n")
-                    pass 
-                else:
-                    pass
+                log.debug("\n=======================================================\n")
+                
                 if (passed.is_set() or failed.is_set()):
-                    ##uncomment for debug
-                    if debugMode:
-                        print("\ntriggered exit in timer\n")
-                        pass 
-                    else:
-                        pass
+                    log.debug("\ntriggered exit in timer\n")
                     raise TimeoutError("you ran out of time")
                     #exit here
                 else:
-                    if debugMode:
-                        print("\nchecking passed........................")
-                        pass 
-                    else:
-                        pass
+                    log.debug("\nchecking passed........................")
                     pass    
-                if debugMode:
-                    print("passed " + str(passed))
-                    pass 
-                else:
-                    pass
+                
+                log.info("passed " + str(passed))
+                
                 print(f"{timerMessage:>100} [{str(i)}]", end="\r") #better formatted
+                
                 time.sleep(abs(step))
+
+
             if ((not passed.is_set()) and (not failed.is_set())):
                 print(" " * (len(timerMessage) + 5))
-                if debugMode:
-                    print("triggering endMessage")
-                    pass 
-                else:
-                    pass
+                
+                log.info("triggering endmesage")
                 print(endMessage, "\n")
                 raise TimeoutError("you ran out of time")
                 
         except TimeoutError:
-            if debugMode:
-                print("timer passed")
-                pass 
-            else:
-                pass
+            log.exception("timeout trigerred")
             return   
         
 if __name__ == "__main__":
